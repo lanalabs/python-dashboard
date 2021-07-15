fa7ca553835ec80567d15f3f400f9090c722cfab
Timo Kubitza <timo.kubitza@lana-labs.com>
2021-07-15 09:21:55 +0200

    adds a shell script to upload new dashboard

---
+++ upload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+API_KEY="<API_KEY>"
+DASHBOARD_ID="<DASHBOARD_ID>"
+URL="<URL>"
+
+rm upload.zip
+cd dashboard; zip -r ../upload.zip *; cd ..
+
+curl -X POST "${URL}/api/v2/custom-dashboards/${DASHBOARD_ID}/source" \
+  -H "Authorization: API-Key ${API_KEY}" \
+  -F file=@"upload.zip"
